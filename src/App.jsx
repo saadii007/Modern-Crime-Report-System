a3df0579b57274bcfcd7b378b9e294bc3bc03dbc
Saad Shabbir <126228618+saadii007@users.noreply.github.com>
2025-05-18 23:06:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <Router>
       <div className="flex flex-col min-h-screen">
         <Navbar />
-        <main className="flex-grow">
+        <main className="flex-grow overflow-x-hidden">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
